1413ecbe401d2237a6e39ca05a86d9ca7e3fd271
Manuela Rink <manuela.schrenk@gmail.com>
2018-06-13 17:20:38 +0200

    new physics lessions for cleanup / sound / interaction

---
+++ ar physics/01 - enable the gravity/01_final/AREasyStart/ViewController.swift
@@ -0,0 +1,330 @@
+//
+//  ViewController.swift
+//  AREasyStart
+//
+//  Created by Manuela Rink on 01.06.18.
+//  Copyright © 2018 Manuela Rink. All rights reserved.
+//
+
+import UIKit
+import SceneKit
+import ARKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var sceneView: ARSCNView!
+    
+    @IBOutlet weak var cleanButton: UIButton!
+    @IBOutlet weak var boxButton: UIButton!
+    @IBOutlet weak var lightButton: UIButton!
+    @IBOutlet weak var candleButton: UIButton!
+    @IBOutlet weak var measureButton: UIButton!
+    
+    @IBOutlet weak var infoBgView: UIView!
+    @IBOutlet weak var infoLabel: UILabel!
+    @IBOutlet weak var distanceLabel: UILabel!
+    @IBOutlet weak var distanceBgView: UIView!
+    
+    let omniLight = SCNLight()
+    let ambientLight = SCNLight()
+    var currentLightEstimate : ARLightEstimate?
+    
+    var measuringNodes: [SCNNode] = []
+    
+    var selectedScenePath : String?
+    
+    var planeDetectionActive = true
+    
+    var screenCenter: CGPoint {
+        let screenSize = view.bounds
+        return CGPoint(x: screenSize.width / 2, y: screenSize.height / 2)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //sceneView.showsStatistics = true
+        boxTapped(boxButton)
+        distanceBgView.isHidden = true
+        infoLabel.text = "All seems good :)"
+        
+        runSession()
+        addLightToScene()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    @IBAction func cleanTapped(_ sender: Any) {
+        removeChildren(inNode: sceneView.scene.rootNode)
+        addLightToScene()
+    }
+    
+    @IBAction func boxTapped(_ sender: UIButton) {
+        selectedScenePath = "art.scnassets/box.scn"
+        selectButton(sender)
+    }
+    
+    @IBAction func lightTapped(_ sender: UIButton) {
+        selectedScenePath = "art.scnassets/lamp.scn"
+        selectButton(sender)
+    }
+    
+    @IBAction func candleTapped(_ sender: UIButton) {
+        selectedScenePath = "art.scnassets/candle.scn"
+        selectButton(sender)
+    }
+    
+    @IBAction func measureTapped(_ sender: UIButton) {
+        selectedScenePath = ""
+        selectButton(sender)
+    }
+    
+    @IBAction func planeDetectionToggle(_ sender: UIButton) {
+        planeDetectionActive = !planeDetectionActive
+        if planeDetectionActive {
+            sender.setTitle("🙈", for: .normal)
+        } else {
+            sender.setTitle("🐵", for: .normal)
+        }
+        
+        runSession()
+    }
+    
+    func selectButton (_ button: UIButton) {
+        [boxButton, lightButton, candleButton, measureButton].forEach { (button) in
+            button?.isSelected = false
+            button?.layer.borderColor = UIColor.clear.cgColor
+            button?.layer.borderWidth = 0
+            distanceBgView.isHidden = true
+        }
+        
+        button.isSelected = true
+        button.layer.borderColor = UIColor.orange.cgColor
+        button.layer.borderWidth = 5
+        
+        if button.tag == 3 {
+            distanceBgView.isHidden = false
+        }
+        
+        print(selectedScenePath ?? "no obj selected")
+    }
+    
+    func addLightToScene () {
+        omniLight.type = .omni
+        omniLight.name = "omniLight"
+        let spotNode = SCNNode()
+        spotNode.light = omniLight
+        spotNode.position = SCNVector3Make(0, 50, 0)
+        
+        sceneView.scene.rootNode.addChildNode(spotNode)
+        
+        ambientLight.type = .ambient
+        ambientLight.name = "ambientLight"
+        let ambientNode = SCNNode()
+        ambientNode.light = ambientLight
+        ambientNode.position = SCNVector3Make(0, 50, 50)
+        sceneView.scene.rootNode.addChildNode(ambientNode)
+    }
+    
+    func runSession() {
+        sceneView.delegate = self
+        let configuration = ARWorldTrackingConfiguration()
+        
+        if planeDetectionActive {
+            if #available(iOS 11.3, *) {
+                configuration.planeDetection = [.horizontal, .vertical]
+            } else {
+                configuration.planeDetection = .horizontal
+            }
+        }
+        
+        configuration.isLightEstimationEnabled = true
+        sceneView.session.run(configuration)
+        
+        //deactivate if not needed!!
+        //can have side effects on other features
+//        sceneView.debugOptions = ARSCNDebugOptions.showFeaturePoints
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if let hit = sceneView.hitTest(screenCenter, types: [.existingPlaneUsingExtent]).first {
+            sceneView.session.add(anchor: ARAnchor(transform: hit.worldTransform))
+            return
+        } else if let hit = sceneView.hitTest(screenCenter, types: [.featurePoint]).last {
+            sceneView.session.add(anchor: ARAnchor(transform: hit.worldTransform))
+            return
+        }
+    }
+    
+    func updateTrackingInfo() {
+        guard let frame = sceneView.session.currentFrame else {
+            return
+        }
+        
+        switch frame.camera.trackingState {
+        case .limited(let reason):
+            switch reason {
+            case .excessiveMotion:
+                infoLabel.text = "Limited Tracking: Excessive Motion"
+            case .insufficientFeatures:
+                infoLabel.text = "Limited Tracking: Insufficient Details"
+            default:
+                infoLabel.text = "Limited Tracking"
+            }
+        default:
+            infoLabel.text = "All seems good :)"
+        }
+        
+        guard let lightEstimate = frame.lightEstimate?.ambientIntensity else {
+            return
+        }
+        
+        currentLightEstimate = frame.lightEstimate
+        
+        if lightEstimate < 100 {
+            infoLabel.text = "Limited Tracking: Too Dark"
+        }
+    }
+    
+    func updateLights () {
+        if let lightInfo = currentLightEstimate {
+            omniLight.intensity = lightInfo.ambientIntensity
+            omniLight.temperature = lightInfo.ambientColorTemperature
+            ambientLight.intensity = lightInfo.ambientIntensity / 2
+            ambientLight.temperature = lightInfo.ambientColorTemperature
+        }
+    }
+    
+    func updateMeasuringNodes() {
+        guard measuringNodes.count > 1 else {
+            return
+        }
+        let firstNode = measuringNodes[0]
+        let secondNode = measuringNodes[1]
+        let showMeasuring = self.measuringNodes.count == 2
+        
+        if showMeasuring {
+            measure(fromNode: firstNode, toNode: secondNode)
+        } else {
+            firstNode.removeFromParentNode()
+            secondNode.removeFromParentNode()
+            measuringNodes.removeFirst(2)
+            distanceLabel.text = ""
+            
+            for node in sceneView.scene.rootNode.childNodes {
+                if node.name == "measuringline" {
+                    node.removeFromParentNode()
+                }
+            }
+        }
+    }
+    
+    func measure(fromNode: SCNNode, toNode: SCNNode) {
+        let measuringLineNode = createLineNode(fromNode: fromNode, toNode: toNode)
+        measuringLineNode.name = "measuringline"
+        sceneView.scene.rootNode.addChildNode(measuringLineNode)
+        
+        let dist = fromNode.position.distanceTo(toNode.position)
+        let measurementValue = String(format: "%.2f", dist)
+        distanceLabel.text = "\(measurementValue) m"
+    }
+    
+}
+
+
+
+
+extension ViewController : ARSCNViewDelegate {
+    
+    func session(_ session: ARSession, didFailWithError error: Error) {
+        infoLabel.text = error.localizedDescription
+    }
+    
+    func sessionWasInterrupted(_ session: ARSession) {
+        infoLabel.text = "Session interuppted :("
+    }
+    
+    func sessionInterruptionEnded(_ session: ARSession) {
+        infoLabel.text = "Session resumed - wait a sec!"
+        for node in sceneView.scene.rootNode.childNodes {
+            node.removeFromParentNode()
+        }
+        runSession()
+    }
+    
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        DispatchQueue.main.async {
+            self.infoLabel.text = ""
+            
+            if let _ = anchor as? ARPlaneAnchor {
+                //do nothing on initial plane detection anymore
+                return
+            } else {
+                if let path = self.selectedScenePath, path.count > 0 {
+                    
+                    let modelClone = SCNScene(named: path)!.rootNode.clone()
+                    if self.boxButton.isSelected {
+                        updatePhysicsOnBoxes(modelClone)
+                    }
+                    
+                    node.addChildNode(modelClone)
+                } else {
+                    //let's measure some stuff!
+                    let measureBubbleNode = createSphereNode(radius: 0.015)
+                    node.addChildNode(measureBubbleNode)
+                    self.measuringNodes.append(node)
+                }
+            }
+        }
+    }
+    
+    func renderer(_ renderer: SCNSceneRenderer, didUpdate node: SCNNode, for anchor: ARAnchor) {
+        DispatchQueue.main.async {
+            if let planeAnchor = anchor as? ARPlaneAnchor {
+                if #available(iOS 11.3, *) {
+                    guard let metalDevice = MTLCreateSystemDefaultDevice() else {
+                        return
+                    }
+                    
+                    let planeGeometry = planeAnchor.geometry
+                    let plane = ARSCNPlaneGeometry(device: metalDevice)
+                    plane?.update(from: planeGeometry)
+                    plane?.firstMaterial?.diffuse.contents = UIColor.clear.cgColor
+//                    plane?.firstMaterial?.diffuse.contents = UIColor(white: 1, alpha: 0.1).cgColor
+//                    plane?.firstMaterial?.colorBufferWriteMask = []
+//                    plane?.firstMaterial?.isDoubleSided = true
+//                    node.castsShadow = false
+//                    node.renderingOrder = -1
+                    node.geometry = plane
+                    node.name = "arplane"
+                    
+                    addPhysicsToPlane(node)
+                }
+            }
+        }
+    }
+    
+    func renderer(_ renderer: SCNSceneRenderer, didRemove node: SCNNode, for anchor: ARAnchor) {
+        guard anchor is ARPlaneAnchor else { return }
+        removeChildren(inNode: node)
+        
+    }
+    
+    func renderer(_ renderer: SCNSceneRenderer, updateAtTime time: TimeInterval) {
+        DispatchQueue.main.async {
+            self.updateTrackingInfo()
+            self.updateLights()
+            self.updateMeasuringNodes()
+        }
+    }
+
+}
---
+++ ar physics/02 - randomize and cleanup/02_final/AREasyStart/NodeUtils.swift
@@ -0,0 +1,135 @@
+import SceneKit
+import ARKit
+import GameplayKit
+
+func nodeWithModelName(_ modelName: String) -> SCNNode {
+    return SCNScene(named: modelName)!.rootNode.clone()
+}
+
+func createPlaneNode(center: vector_float3, extent: vector_float3) -> SCNNode {
+    let plane = SCNPlane(width: CGFloat(extent.x), height: CGFloat(extent.z))
+    
+    let planeMaterial = SCNMaterial()
+    planeMaterial.diffuse.contents = UIColor.blue.withAlphaComponent(0.4)
+    plane.materials = [planeMaterial]
+    let planeNode = SCNNode(geometry: plane)
+    planeNode.position = SCNVector3Make(center.x, 0, center.z)
+    planeNode.transform = SCNMatrix4MakeRotation(-Float.pi / 2, 1, 0, 0)
+    
+    return planeNode
+}
+
+func updatePlaneNode(_ node: SCNNode, center: vector_float3, extent: vector_float3) {
+    let geometry = node.geometry as! SCNPlane
+    
+    geometry.width = CGFloat(extent.x)
+    geometry.height = CGFloat(extent.z)
+    node.position = SCNVector3Make(center.x, 0, center.z)
+}
+
+func addPhysicsToPlane(_ node: SCNNode) {
+    if #available(iOS 11.3, *) {
+        let geo = node.geometry as! ARSCNPlaneGeometry
+        let boundingSphere = geo.boundingSphere
+
+        if let plane = node.childNode(withName: "physicsplane", recursively: true) {
+            plane.removeFromParentNode()
+        }
+
+        let plane = SCNPlane(width: CGFloat(boundingSphere.radius), height: CGFloat(boundingSphere.radius))
+        plane.cornerRadius = 50
+
+        let planeMaterial = SCNMaterial()
+        planeMaterial.diffuse.contents = UIColor.red.withAlphaComponent(0.3)
+        plane.materials = [planeMaterial]
+
+        let planeNode = SCNNode(geometry: plane)
+        planeNode.name = "physicsplane"
+        planeNode.position = SCNVector3Make(boundingSphere.center.x, 0, boundingSphere.center.z)
+        planeNode.transform = SCNMatrix4MakeRotation(-Float.pi / 2, 1, 0, 0)
+
+        node.addChildNode(planeNode)
+
+        planeNode.physicsBody = nil
+        planeNode.physicsBody = SCNPhysicsBody.kinematic()
+        planeNode.physicsBody?.physicsShape = SCNPhysicsShape(geometry: plane, options: nil)
+        planeNode.physicsBody?.mass = 100 //mass in kg!
+        planeNode.physicsBody?.restitution = 0.0
+        planeNode.physicsBody?.friction = 1.0
+    }
+}
+
+func updatePhysicsOnBoxes (_ model: SCNNode) {
+    let boxNode = model.childNode(withName: "mybox", recursively: true)
+    if let node = boxNode {
+        var scale = GKRandomSource.sharedRandom().nextUniform()
+        scale = scale < 0.5 ? 0.5 : scale
+        node.scale = SCNVector3(x: scale, y: scale, z: scale)
+        node.physicsBody = nil
+        
+        let physicsBody = SCNPhysicsBody.dynamic()
+        physicsBody.mass = CGFloat(scale)
+        physicsBody.restitution = 0.1
+        physicsBody.friction = 0.75
+        physicsBody.damping = 0.3
+        physicsBody.categoryBitMask = CollisionTypes.shape.rawValue
+        node.physicsBody = physicsBody
+        node.position.y = 0.2
+    }
+}
+
+func removeChildren(inNode node: SCNNode) {
+    for node in node.childNodes {
+        if let name = node.name, name.contains("plane") {
+            //found plane - do nothing ;)
+        } else {
+            node.removeFromParentNode()
+        }
+    }
+}
+
+func createSphereNode(radius: CGFloat) -> SCNNode {
+    let sphere = SCNSphere(radius:radius)
+    sphere.firstMaterial?.diffuse.contents = UIColor.red
+    return SCNNode(geometry: sphere)
+}
+
+func createLineNode(fromNode: SCNNode, toNode: SCNNode) -> SCNNode {
+    let line = lineFrom(vector: fromNode.position, toVector: toNode.position)
+    let lineNode = SCNNode(geometry: line)
+    let planeMaterial = SCNMaterial()
+    planeMaterial.diffuse.contents = UIColor.red
+    line.materials = [planeMaterial]
+    return lineNode
+}
+
+func lineFrom(vector vector1: SCNVector3, toVector vector2: SCNVector3) -> SCNGeometry {
+    let indices: [Int32] = [0, 1]
+    
+    let source = SCNGeometrySource(vertices: [vector1, vector2])
+    let element = SCNGeometryElement(indices: indices, primitiveType: .line)
+    
+    return SCNGeometry(sources: [source], elements: [element])
+}
+
+func - (left: SCNVector3, right: SCNVector3) -> SCNVector3 {
+    return SCNVector3Make(left.x - right.x, left.y - right.y, left.z - right.z)
+}
+
+struct CollisionTypes : OptionSet {
+    let rawValue: Int
+    
+    static let bottom  = CollisionTypes(rawValue: 1 << 0)
+    static let shape = CollisionTypes(rawValue: 1 << 1)
+}
+
+extension SCNVector3 {
+    
+    func length() -> Float {
+        return sqrtf(x * x + y * y + z * z)
+    }
+    
+    func distanceTo(_ vector: SCNVector3) -> Float {
+        return SCNVector3(x: self.x - vector.x, y: self.y - vector.y, z: self.z - vector.z).length()
+    }
+}
