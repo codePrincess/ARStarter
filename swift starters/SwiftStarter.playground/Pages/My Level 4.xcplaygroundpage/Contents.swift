a3fda20d1e320cf67f2dfaa3662afecfda512ba2
Manu Rink <rink.manu@gmail.com>
2019-04-26 16:35:28 +0200

    add new content to swift starter
    Co-Authored-By: manu rink <codeprincess@users.noreply.github.com>

---
+++ swift starters/SwiftStarter.playground/Pages/My Level 4.xcplaygroundpage/Contents.swift
@@ -0,0 +1,35 @@
+import Foundation
+
+
+/*:
+ # Level 4 - The Bonus
+ 
+ Oben drauf gibt es nun etwas Zucker. Zum einen sehen wir uns an, was Optionals sind, wie sie funktionieren, aber vor allem wie sie unseren Code sicherer gestalten.
+ 
+ In dem Zuge werfen wir auch gleich ein Auge auf Typinferenz - ein Feature, dass eins der grundlegenden Basiskonzepte von Swift ist. Typen werden implizit eruiert und nur wenn es kein eindeutiges Ergebnis gibt, muss man selbst Typen nennen.
+ 
+ * callout(Level 4 - The Bonus):
+ - A. Optionals
+ - B. Nebenschauplatz Typinferenz
+ - C. Error Handling
+ */
+
+
+/*
+ A. Optionals ?!
+ 
+ In Swift sind Optionals ein tolles Werkzeug, um sicher mit etwaig leeren Werten umzugehen.
+ Grundsätzlich gibt es Swift keine leeren Werte - Variablen und Konstanten werden per se als initialisiert und mit einem Wert belegt angenommen. Da dies aber nicht immer praktikabel und umsetzbar ist, wurden Optionals ins Leben gerufen
+ */
+
+
+
+/*
+ B. Typinferenz - ein Feature, dass eins der grundlegenden Basiskonzepte von Swift ist. Typen werden implizit eruiert und nur wenn es kein eindeutiges Ergebnis gibt, muss man selbst Typen nennen.
+ */
+
+
+
+/*
+ C. Error Handling -  Wie kann man Fehler werfen, diese selbst definieren und auch im Programm darauf reagieren - etwa sogar mit Optionals?
+ */
