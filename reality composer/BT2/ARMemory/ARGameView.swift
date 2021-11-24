c88ef5689233c2e78401c8af92befa93544a7840
Manu Rink <manuela.rink@apple.com>
2021-11-24 11:44:47 +0100

    new section 3 for getting started with xcode <-> reality composer

---
+++ reality composer/BT2/ARMemory/ARGameView.swift
@@ -0,0 +1,140 @@
+//
+//  ARGameView.swift
+//  ARMemory
+//
+//  Created by Manu Rink on 07.11.21.
+
+/*
+ 
+ Available functionality:
+ ------------------------
+ 
+ 1) Flip a card
+    Every card has a notification ON TAP installed on
+    the RC side called "cupFlip" and is the same for
+    every single card.
+    We register for the action and let the "handleTapOnEntity"
+    method deal with what happens then - currently we
+    just flip the card 180° so we can easily toggle the status
+ 
+ 2) Flip the whole board
+    There is a notification installed on the RC side which
+    we can call from our code. It's called "flipTheBoard"
+    and currently we call that directly from our native
+    SwiftUI button and not from any 3D model interaction.
+    Yes - that is really cool, I know 😅.
+ 
+ 3) Comparing logic
+    For knowing if we flipped two similar cards we need to
+    memorize our cards
+ 
+ 4) Only two cards are allowed to be flipped to visible at once
+    As soon as more than two cards are visible we'll turn the
+    first two to invisible again.
+ 
+ 5) We know when the game is finished.
+    So when all cards are successfully turned by pairs of two
+    in a row.
+ 
+ 6) We count our turns and measure the elapsed time
+    Did we got better? That's why we need to count how many
+    turns we needed until finishing the game plus we measure the time
+ 
+ */
+
+import Foundation
+import RealityKit
+import ARKit
+
+class ARGameView : ARView, ARSessionDelegate {
+    var uiViewState : ARViewState!
+    
+    var boardAnchor : Experience.GameBoard!
+    var lastFlippedCard: Entity?
+    
+    var pairsFound = 0
+    private let pairsNeeded = 2
+    var tapCount : Int = 0
+    
+    func setupGameBoard () {
+        // Load the "Box" scene from the "Experience" Reality File
+        boardAnchor = try! Experience.loadGameBoard()
+        
+        //register all needed actions from RC project
+        boardAnchor.actions.cupFlip.onAction = handleTapOnEntity(_:)
+        
+        let occlusionMaterial = OcclusionMaterial()
+        //let simpleMaterial = SimpleMaterial(color: UIColor.red.withAlphaComponent(1), isMetallic: false)
+        
+        let occlusionBox = ModelEntity(mesh: .generateBox(width: 0.5, height: 0.2, depth: 0.5), materials: [occlusionMaterial])
+        occlusionBox.transform.translation -= SIMD3<Float>(0, 0.1, 0)
+        
+        // Add the box anchor to the scene
+        boardAnchor.addChild(occlusionBox)
+        self.scene.anchors.append(boardAnchor)
+    }
+    
+    func turnAllCards() -> GameState {
+        boardAnchor.notifications.flipTheBoard.post()
+        print("will turn all cards")
+        
+        return .running
+    }
+    
+    func handleTapOnEntity(_ entity: Entity?) {
+        
+        if let animatable = entity {
+            
+            //if game is not started yet no cards can be turned
+            if !(uiViewState.gameState == .running) {
+                return
+            }
+            
+            //check if same card is touched again
+            if sameCardTouchedAgain(first: animatable, second: lastFlippedCard) {
+                return
+            }
+            
+            showCard(entity: animatable)
+            
+            if pairFound(first: animatable, second: lastFlippedCard) {
+                print(" +++ PAIR FOUND -> \(animatable.name)")
+                pairsFound += 1
+                
+                
+                DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+                    self.lastFlippedCard?.removeFromParent()
+                    animatable.removeFromParent()
+                    self.lastFlippedCard = nil
+                }
+                
+                if pairsFound == pairsNeeded {
+                    uiViewState.gameState = .finished
+                }
+
+            }
+            //no pair found
+            else {
+                //two cards turned - hide both cards again and reset lastFlippedCard
+                if let card = lastFlippedCard {
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+                        hideCard(entity: card)
+                        hideCard(entity: animatable)
+                    }
+                    lastFlippedCard = nil
+                //only one card turned - memorize as lastflippedcard
+                } else {
+                    lastFlippedCard = animatable
+                }
+            }
+            
+            tapCount += 1
+            uiViewState.turns = tapCount/2
+            
+        }
+        
+    }
+    
+    
+
+}
---
+++ reality composer/BT2/ARMemory/ContentView.swift
@@ -0,0 +1,126 @@
+//
+//  ContentView.swift
+//  ARMemory
+//
+//  Created by Manu Rink on 07.11.21.
+//
+
+import SwiftUI
+import RealityKit
+
+struct ContentView : View {
+    
+    @StateObject var arViewState = ARViewState()
+    
+    var body: some View {
+        ZStack {
+            ARViewContainer(viewState: arViewState).edgesIgnoringSafeArea(.all)
+            
+            VStack {
+                Spacer()
+                
+                if arViewState.gameState == .notStarted {
+                    VStack {
+                        Text("Welcome to AR Memory 🙌")
+                            .foregroundColor(.white)
+                            .font(.title2)
+                        Spacer()
+                        Text("Hit the start button and challenge yourself with a decent game of memory direc tly where you are in AR.")
+                            .foregroundColor(Color(.systemGray2))
+                            .font(.body)
+                        
+                        Spacer()
+                        
+                        VStack {
+                            Button("Start game") {
+                                arViewState.turnTable()
+                            }
+                            .padding()
+                            .background(Color.white)
+                            .foregroundColor(.black)
+                            .font(.title2)
+                            .cornerRadius(10)
+                        }
+                        
+                        Spacer(minLength: 5)
+                    }
+                    .frame(width: 300, height: 250)
+                    .padding()
+                    .background(Color.black)
+                    .cornerRadius(15)
+                }
+                
+                if arViewState.gameState == .finished {
+                    
+                    VStack {
+                        Text("Congratulations 🎉")
+                            .foregroundColor(.black)
+                            .font(.title2)
+                        
+                        Spacer()
+                        
+                        Text("You finished the game in no time. Genius!\nYou just turned \(arViewState.turns) pairs of cards and succeeded.\nWanna try again?")
+                            .foregroundColor(Color(.systemGray))
+                            .font(.body)
+                        
+                        Spacer()
+                        
+                        VStack {
+                            Button("Start again") {}
+                            .padding()
+                            .background(Color.black)
+                            .foregroundColor(.white)
+                            .font(.title2)
+                            .cornerRadius(10)
+                        }
+                        
+                        Spacer(minLength: 5)
+                    }
+                    .frame(width: 300, height: 250)
+                    .padding(20)
+                    .background(Color.white)
+                    .cornerRadius(15)
+                }
+                
+            }
+        }
+    }
+}
+
+class ARViewState : ObservableObject {
+    
+    weak var arView : ARGameView?
+    @Published var gameState : GameState = .notStarted
+    @Published var turns : Int = 0
+    
+    func turnTable () {
+        gameState = arView!.turnAllCards()
+    }
+    
+}
+
+struct ARViewContainer: UIViewRepresentable {
+    
+    @StateObject var viewState : ARViewState
+    
+    func makeUIView(context: Context) -> ARView {
+        let arView = ARGameView(frame: .zero)
+        arView.uiViewState = viewState
+        
+        arView.setupGameBoard()
+        viewState.arView = arView
+        
+        return arView
+    }
+    
+    func updateUIView(_ uiView: ARView, context: Context) {}
+    
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
